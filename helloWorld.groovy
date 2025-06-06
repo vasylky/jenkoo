b79cb12df9da6a9bff54a0265650291780f39101
vasylky <vasylqvky@gmail.com>
2025-06-06 12:52:05 +0300

    Fix helloWorld.groovy syntax for Job DSL

--- helloWorld.groovy
+++ helloWorld.groovy
@@ -1,21 +1,19 @@
-jobDsl {
-    pipelineJob('MyProjectPipeline') {
-        description('Pipeline for MyProject')
-        parameters {
-            stringParam('PROJECT_NAME', 'MyProject', 'Name of the project')
-            stringParam('VERSION', '1.0.0', 'Version of the project')
-        }
-        definition {
-            cpsScm {
-                scm {
-                    git {
-                        remote {
-                            url('https://github.com/vasylky/jenkoo.git')
-                            branch('main')
-                        }
+pipelineJob('MyProjectPipeline') {
+    description('Pipeline for MyProject')
+    parameters {
+        stringParam('PROJECT_NAME', 'MyProject', 'Name of the project')
+        stringParam('VERSION', '1.0.0', 'Version of the project')
+    }
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/vasylky/jenkoo.git')
+                        branch('main')
                     }
-                    scriptPath('Jenkinsfile')
                 }
+                scriptPath('Jenkinsfile')
             }
         }
     }
