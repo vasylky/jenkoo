43953c1609d85ddd0c29757dc46899eaea6b05f2
vasylky <vasylqvky@gmail.com>
2025-06-10 17:38:41 +0300

    change to pipe

--- pythonFunc.groovy
+++ pythonFunc.groovy
@@ -4,6 +4,8 @@ pipelineJob('MyPythonFunc') {
         stringParam('PROJECT_NAME', 'PyProject', 'Name of the project')
         stringParam('VERSION', '1.2.0', 'Version of the project')
         choiceParam('BRANCH_NAME', ['main', 'feature/test'], 'Git branch to build')
+        stringParam('NUM1', '5', 'First number')
+        stringParam('NUM2', '10', 'Second number')
     }
     definition {
         cpsScm {
